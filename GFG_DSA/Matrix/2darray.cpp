af68d1bf49b5ff76d85da3ef72f9ca5c539e6ead
Prabhjot Singh Bedi <prabhjotbedi75@gmail.com>
2021-12-14 13:08:31 +0530

    C++ Program

---
+++ GFG_DSA/Matrix/2darray.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+int i,j;
+int arr[3][2]={{1,2},
+               {3,4},
+               {5,6}};
+               
+for(i=0;i<3;i++){
+    for(j=0;j<2;j++){
+        cout<<arr[i][j];
+    }
+}
+
+}
